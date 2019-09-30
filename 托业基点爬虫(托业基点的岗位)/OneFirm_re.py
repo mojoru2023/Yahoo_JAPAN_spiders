380e258252c92515896b94ee0a9c8dff5cb4ae96
Karson <34601389+Greenbirch2007@users.noreply.github.com>
2019-09-30 08:06:17 +0800

    Update OneFirm_re.py

--- 托业基点爬虫(托业基点的岗位)/OneFirm_re.py
+++ 托业基点爬虫(托业基点的岗位)/OneFirm_re.py
@@ -35,12 +35,20 @@ from selenium import webdriver
 
 
 def call_page(url):
-    options = webdriver.ChromeOptions()
-    options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome("/usr/bin/chromedriver", chrome_options=options)
-    driver.get(url)
-    html = driver.page_source
-    return html
+    # options = webdriver.ChromeOptions()
+    # options.add_argument("--no-sandbox")
+    # driver = webdriver.Chrome("/usr/bin/chromedriver", chrome_options=options)
+    # driver.get(url)
+    # html = driver.page_source
+    # return html
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
+        return None
+
 
 
 
@@ -98,39 +106,36 @@ def insertDB(content):
     except TypeError :
         pass
 
-#
-# if __name__ == '__main__':
-#     # 使用cursor()方法获取操作游标
-#     connection = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='123456', db='Yahoo_J',
-#                                  charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
-#     cur = connection.cursor()
-#     #sql 语句
-#     for i in range(1,3147):
-#         sql = 'select * from allFirm_toeic where id = %s ' % i
-#         # #执行sql语句
-#         cur.execute(sql)
-#         # #获取所有记录列表
-#         data = cur.fetchone()
-#         url_str = data['link']
-#
-#
-#         html = call_page(url_str)
-#         try:
-#             big_list =[]
-#             content = parse_html(html)
-#
-#             print(content,url_str)
-#
-#
-#             print(datetime.datetime.now())
-#             time.sleep(6)
-#         except ValueError :
-#             pass
 
-url = 'https://job.yahoo.co.jp/jobs/detail/d40b59506e952926a542520ef26ec5844a73cb8d6c1896be0e43f9323b37d9bc?keyword=TOEIC&l=%E6%9D%B1%E4%BA%AC%E9%83%BD&side=1'
-html =call_page(url)
-content = parse_html(html)
-print(content,url)
+if __name__ == '__main__':
+    # 使用cursor()方法获取操作游标
+    connection = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='123456', db='Yahoo_J',
+                                 charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+    cur = connection.cursor()
+    #sql 语句
+    for i in range(1,1960):
+        sql = 'select * from allFirm_toeic where id = %s ' % i
+        # #执行sql语句
+        cur.execute(sql)
+        # #获取所有记录列表
+        data = cur.fetchone()
+        url_str = data['link']
+
+
+        html = call_page(url_str)
+        try:
+            big_list =[]
+            content = parse_html(html)
+
+            print(content,url_str)
+
+
+            print(datetime.datetime.now())
+            time.sleep(1)
+        except ValueError :
+            pass
+
+
 
 
 # create table OneFirm_toeic(
